943aa68bd32af5b72d2923cf4aaaae94e6dc41a1
shahiyan7 <shahiyan.21scse1010240@galgotiasuniversity.edu.in>
2023-04-06 21:27:50 +0530

    modified even no of digit problem

--- Java/Linear_search/src/LecQuestions/EvenNumberofDigits.java
+++ Java/Linear_search/src/LecQuestions/EvenNumberofDigits.java
@@ -3,30 +3,31 @@ package LecQuestions;
 public class EvenNumberofDigits {
     public static void main(String[] args) {
 //        method-1
-        int[] nums={1,55,23,9,345,66};
-        int ans=EvenDigits(nums);
+        int[] nums = {1, 55, 23, 9, 345, 66};
+        int ans = EvenDigits(nums);
         System.out.println(ans);
 
 //        method-2 for finding even number of digits
-        int[] num2={12,234,6754,1,289};
-        int ans2=EvenDigits(num2);
+        int[] num2 = {12, 234, 6754, 1, 289};
+        int ans2 = EvenDigits(num2);
         System.out.println(ans2);
     }
-    static int EvenDigits(int[] arr){
-        int count=0;
+
+    static int EvenDigits(int[] arr) {
+        int count = 0;
         for (int i = 0; i < arr.length; i++) {
-            String num=String.valueOf(arr[i]);
-            if(num.length()%2==0){
+            String num = String.valueOf(arr[i]);
+            if (num.length() % 2 == 0) {
                 count++;
             }
         }
         return count;
     }
 
-    static int EvenDigit2(int [] arr){
-        int count=0;
+    static int EvenDigit2(int[] arr) {
+        int count = 0;
         for (int i = 0; i < arr.length; i++) {
-            if(even(arr[i])){
+            if (even(arr[i])) {
                 count++;
             }
         }
@@ -34,12 +35,12 @@ public class EvenNumberofDigits {
     }
 
     private static boolean even(int n) {
-        int count=0;
-        while (n>0){
+        int count = 0;
+        while (n > 0) {
             count++;
-            n=n/10;
+            n = n / 10;
         }
-        if(count%2==0){
+        if (count % 2 == 0) {
             return true;
         }
         return false;
