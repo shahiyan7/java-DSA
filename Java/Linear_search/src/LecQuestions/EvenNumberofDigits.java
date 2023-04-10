2cce7c9b3d8d0238340705ef78bad8d9cfadf81c
shahiyan7 <shahiyan.21scse1010240@galgotiasuniversity.edu.in>
2023-04-10 23:38:29 +0530

    optimized solution for even no of digits

--- Java/Linear_search/src/LecQuestions/EvenNumberofDigits.java
+++ Java/Linear_search/src/LecQuestions/EvenNumberofDigits.java
@@ -3,26 +3,27 @@ package LecQuestions;
 public class EvenNumberofDigits {
     public static void main(String[] args) {
 //        method-1
-        int[] nums = {1, 55, 23, 9, 345, 66};
-        int ans = EvenDigits(nums);
-        System.out.println(ans);
+//        int[] nums = {1, 55, 23, 9, 345, 66};
+//        int ans = EvenDigits(nums);
+//        System.out.println(ans);
 
 //        method-2 for finding even number of digits
         int[] num2 = {12, 234, 6754, 1, 289};
-        int ans2 = EvenDigits(num2);
+        int ans2 = EvenDigit2(num2);
         System.out.println(ans2);
+        System.out.println(digits(4567));
     }
 
-    static int EvenDigits(int[] arr) {
-        int count = 0;
-        for (int i = 0; i < arr.length; i++) {
-            String num = String.valueOf(arr[i]);
-            if (num.length() % 2 == 0) {
-                count++;
-            }
-        }
-        return count;
-    }
+//    static int EvenDigits(int[] arr) {
+//        int count = 0;
+//        for (int i = 0; i < arr.length; i++) {
+//            String num = String.valueOf(arr[i]);
+//            if (num.length() % 2 == 0) {
+//                count++;
+//            }
+//        }
+//        return count;
+//    }
 
     static int EvenDigit2(int[] arr) {
         int count = 0;
@@ -35,14 +36,20 @@ public class EvenNumberofDigits {
     }
 
     private static boolean even(int n) {
+        int numofDigits=digits(n);
+
+        if (numofDigits % 2 == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    static int digits(int n){
         int count = 0;
         while (n > 0) {
             count++;
             n = n / 10;
         }
-        if (count % 2 == 0) {
-            return true;
-        }
-        return false;
+        return count;
     }
 }
