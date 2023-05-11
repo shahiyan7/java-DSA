44b78d4454fbe14e33f8e001e323758df1944673
shahiyan7 <shahiyan.21scse1010240@galgotiasuniversity.edu.in>
2023-05-11 16:15:18 +0530

    method 2 for ceiling problem

--- Java/Binary_Search/src/leetCodeQuestion/CeilingProblem.java
+++ Java/Binary_Search/src/leetCodeQuestion/CeilingProblem.java
@@ -5,7 +5,9 @@ public class CeilingProblem {
         int[] arr={2,3,5,9,14,16,18};
         int target=6;
         int ans=ceilingNumber(arr,target);
+        int ans2=ceilingNumber2(arr,15);
         System.out.println(ans);
+        System.out.println(ans2);
     }
     static  int ceilingNumber(int[] arr,int target){
         int start=0;
@@ -28,5 +30,24 @@ public class CeilingProblem {
             }
         }
         return ans;
+    }
+
+
+    static  int ceilingNumber2(int[] arr,int target){
+        int start=0;
+        int end= arr.length-1;
+        while (start<=end){
+            int mid=start+(end-start)/2;
+            if(target<arr[mid]){
+              end=mid-1;
+            }
+            else if(target>arr[mid]){
+                start=mid+1;
+            }
+            else {
+                return  arr[mid];
+            }
+        }
+        return arr[start];
     }
 }
