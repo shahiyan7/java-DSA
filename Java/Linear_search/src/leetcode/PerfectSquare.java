70ca25536294e388cf13eed8b8101a9004616c24
shahiyan7 <shahiyan.21scse1010240@galgotiasuniversity.edu.in>
2023-05-13 11:33:53 +0530

    unexpected error

--- Java/Linear_search/src/leetcode/PerfectSquare.java
+++ Java/Linear_search/src/leetcode/PerfectSquare.java
@@ -1,2 +1,22 @@
-package leetcode;public class PerfectSquare {
+package leetcode;
+
+import java.util.Scanner;
+
+public class PerfectSquare {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int num=in.nextInt();
+        System.out.println(isPerfectSquare(num));
+    }
+    static boolean isPerfectSquare(int n){
+        for (int i = 1; i < n/2; i++) {
+            int square=i*i;
+            if(square==n){
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
