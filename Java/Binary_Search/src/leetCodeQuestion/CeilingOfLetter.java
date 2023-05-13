19b220d37cbe7a90894031a76340e5c591b559b0
shahiyan7 <shahiyan.21scse1010240@galgotiasuniversity.edu.in>
2023-05-13 17:38:45 +0530

    updated ceilingOfLetter

--- Java/Binary_Search/src/leetCodeQuestion/CeilingOfLetter.java
+++ Java/Binary_Search/src/leetCodeQuestion/CeilingOfLetter.java
@@ -3,26 +3,26 @@ package leetCodeQuestion;
 public class CeilingOfLetter {
     public static void main(String[] args) {
         Character[] arr={'c','f','j'};
-       char target='a';
+       char target='j';
        char ans=smallestLetter(arr,target);
         System.out.println(ans);
     }
 
    static char smallestLetter(Character[] arr, char target) {
-        if(target>=arr[arr.length-1]){
-            return arr[0];
-        }
+//        if(target>=arr[arr.length-1]){      you can do this also but there is also a better approch using modulo..
+//            return arr[0];
+//        }
         int start=0;
         int end =arr.length-1;
         while(start<=end){
             int mid =start+(end-start)/2;
-            if(target>arr[mid]){
-                start=mid+1;
-            }
-            else {
+            if(target<arr[mid]){
                 end=mid-1;
             }
+            else {
+                start=mid+1;
+            }
         }
-        return arr[start];
+        return arr[start % arr.length];
    }
 }
