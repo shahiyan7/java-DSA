a24049a70d783402167f1b41fa27cd2f42e9ee50
shahiyan7 <shahiyan.21scse1010240@galgotiasuniversity.edu.in>
2023-05-13 17:23:17 +0530

    updated ceilingOfLetter

--- Java/Binary_Search/src/leetCodeQuestion/CeilingOfLetter.java
+++ Java/Binary_Search/src/leetCodeQuestion/CeilingOfLetter.java
@@ -3,12 +3,15 @@ package leetCodeQuestion;
 public class CeilingOfLetter {
     public static void main(String[] args) {
         Character[] arr={'c','f','j'};
-       char target='d';
+       char target='a';
        char ans=smallestLetter(arr,target);
         System.out.println(ans);
     }
 
    static char smallestLetter(Character[] arr, char target) {
+        if(target>=arr[arr.length-1]){
+            return arr[0];
+        }
         int start=0;
         int end =arr.length-1;
         while(start<=end){
