753474e2399066add36136ca9d2dc3c399397352
shahiyan7 <shahiyan.21scse1010240@galgotiasuniversity.edu.in>
2023-05-16 10:34:26 +0530

    added solution for the PeakIndexMountainArray

--- Java/Binary_Search/src/leetCodeQuestion/PeakIndexMountainArray.java
+++ Java/Binary_Search/src/leetCodeQuestion/PeakIndexMountainArray.java
@@ -1,4 +1,24 @@
 package leetCodeQuestion;
 
 public class PeakIndexMountainArray {
+    public static void main(String[] args) {
+        int[] arr={0,2,1,0};
+        System.out.println(peakIndex(arr));
+    }
+    public  static int peakIndex(int[] arr){
+        int ans=-1;
+        int start=0;
+        int end=arr.length-1;
+        while (start<=end){
+            int mid=start +(end-start)/2;
+            if(arr[mid]<arr[mid+1]){
+                start=mid+1;
+            }
+            else{
+                ans=mid;
+                end=mid-1;
+            }
+        }
+        return ans;
+    }
 }
