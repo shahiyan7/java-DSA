5dd58423d3808ca564d6cc660d29c880149b9215
shahiyan7 <shahiyan.21scse1010240@galgotiasuniversity.edu.in>
2023-06-07 10:25:19 +0530

    optimized code for bubble sort

--- Java/Bubble_sort/src/LecQuestion/BubbleSort.java
+++ Java/Bubble_sort/src/LecQuestion/BubbleSort.java
@@ -5,20 +5,25 @@ import java.util.Arrays;
 public class BubbleSort {
     public static void main(String[] args) {
         int[] arr={3,1,5,4,2};
-        int searchIndex= arr.length-1;
-        for (int j = 0; j < arr.length-1 ; j++) {
-            for (int i = 0; i < searchIndex; i++) {
-                if(arr[i]>arr[i+1]){
-                    int temp=arr[i];
-                    arr[i]=arr[i+1];
-                    arr[i+1]=temp;
+        bubble(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void bubble(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            boolean swapped=false;
+            for (int j = 1; j < arr.length-i; j++) {
+                if(arr[j]<arr[j-1]){
+                    int temp=arr[j-1];
+                    arr[j-1]=arr[j];
+                    arr[j]=temp;
+                    swapped=true;
                 }
 
             }
-            searchIndex--;
+            if(!swapped){
+                break;
+            }
         }
-
-        System.out.println(Arrays.toString(arr));
     }
 
 }
--- Java/Bubble_sort/src/Main.java
+++ Java/Bubble_sort/src/Main.java
@@ -1,5 +1,27 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+      int arr[]={5,4,3,2,1};
+        bubble(arr);
+        System.out.println(Arrays.toString(arr));
     }
+    static void bubble(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+          boolean swapped=false;
+            for (int j = 1; j < arr.length-i; j++) {
+                if(arr[j]<arr[j-1]){
+                    int temp=arr[j-1];
+                    arr[j-1]=arr[j];
+                    arr[j]=temp;
+                    swapped=true;
+                }
+
+            }
+            if(!swapped){
+                break;
+            }
+        }
+    }
+
 }
