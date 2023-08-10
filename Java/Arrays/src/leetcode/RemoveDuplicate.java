26da6b8109aab5d3308b9b39da223f62bd60aceb
shahiyan7 <shahiyan.21scse1010240@galgotiasuniversity.edu.in>
2023-08-10 11:28:09 +0530

    addded the pattern questions

---
+++ Java/Arrays/src/leetcode/RemoveDuplicate.java
@@ -0,0 +1,23 @@
+package leetcode;
+
+import java.util.HashSet;
+
+public class RemoveDuplicate {
+    public static void main(String[] args) {
+        int[] nums={1,1,2,3,4,1,2,5};
+        int ans=removeDuplicates(nums);
+        System.out.println(ans);
+
+    }
+
+   static public int removeDuplicates(int[] nums) {
+        int k=0;
+       int[] uniqueNums=new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+
+        }
+        return k;
+    }
+}
+
+
