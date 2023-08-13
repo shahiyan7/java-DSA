c45c744deb5811ace0358aeb01390f2e37e4d807
shahiyan7 <shahiyan.21scse1010240@galgotiasuniversity.edu.in>
2023-08-13 11:16:14 +0530

    added AddToArrayForm.problem

--- Java/Arrays/src/leetcode/AddToArrayForm.java
+++ Java/Arrays/src/leetcode/AddToArrayForm.java
@@ -1,7 +1,29 @@
 package leetcode;
 
+import java.util.ArrayList;
+
 public class AddToArrayForm {
     public static void main(String[] args) {
-
+     int[] num ={1,2,0,0};
+     int k=34;
+     ArrayList<Integer> ans= addToArrayForm(num,k);
+        System.out.println(ans);
     }
+
+    public static ArrayList<Integer> addToArrayForm(int[] num, int k){
+        ArrayList<Integer> myAnswer=new ArrayList<>();
+        String stringnumber="";
+        for (int i = 0; i < num.length ; i++) {
+            stringnumber=stringnumber+Integer.toString(num[i]);
+        }
+        int finalnum=Integer.parseInt(stringnumber)+k;
+         while (finalnum>0){
+             int digit=finalnum%10;
+             myAnswer.add(0,digit);
+             finalnum=finalnum/10;
+         }
+        return myAnswer;
+    }
+
+
 }
